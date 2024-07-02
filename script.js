1be71f8e97e70524c48fe722a91cada1bd996c1f
darkfrozen132 <darkfrozen@outlook.com>
2024-07-01 23:31:03 -0500

    23:30

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-let b=prompt('ingrese un numero');
+function hallarporcentajeimpuesto(){
+var numero1=parseFloat(document.getElementById('numero1').value);
+var numero2=parseFloat(document.getElementById('numero2').value);
+
+var impuesto=numero1*(1+numero2/100);
+
+//alert("La suma de los numeros es :"+suma);
+
+    document.getElementById('resultado').innerText = impuesto;
+
+
+
+}
